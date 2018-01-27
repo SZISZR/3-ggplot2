b00fdb4668fb6bdbf325d6f9dab1a87c44e6d54f
Tímea Tóth <tiemka14@gmail.com>
2018-01-28 00:17:57 +0100

    ggplot2 alapok, to do hf

---
+++ hf.R
@@ -0,0 +1,7 @@
+# A házifeladatok beadásánál elég scriptet küldeni.
+
+# 1. A plot függvény segítségével állítsatok elõ vonaldiagramot 
+# a várható élettartam alakulásáról Ausztriában a 2000 elõtti idõszakra vonatkozóan
+
+# 2. Ábrazoljuk a 2000-es évektõl Európában a medián és az átlagos várható élettartam alakulását évenként
+# Hint: a medián és az átlagos várható élettartam két különbözõ rétegben ábrázolható
